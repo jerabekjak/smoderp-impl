40d2adee90e19c39cddc53db26d141eb69c5d545
jerabekjak <jerabekjak@email.cz>
2018-10-25 14:59:34 +0200

    kosmetika

--- smoderp2d/iter_crit.py
+++ smoderp2d/iter_crit.py
@@ -1,13 +1,6 @@
 # @package smoderp2d.courant defines iteration criterion
 
-
-import math
-import smoderp2d.constants as constants
-from smoderp2d.tools.tools import comp_type
-from smoderp2d.tools.tools import get_argv
-import smoderp2d.io_functions.prt as prt
 from smoderp2d.exceptions import MaxIterationExceeded
-
 from smoderp2d.core.General import Globals as Gl
 
 
@@ -30,32 +23,32 @@ class IterCrit():
     def reset(self):
         self.pre_rill_count = 0
         self.iter_ = 0
-    
+
     # Increase interataion count
-    # 
+    #
     def iter_up(self):
         self.iter_ += 1
-    
+
     # Check max iteration reached
-    def crit_iter_check(self,total_time):
+    # 
+    def crit_iter_check(self, total_time):
         if (self.iter_ >= self.crit_iter_):
             return True
             #raise MaxIterationExceeded(self.crit_iter_, total_time)
-    
+
     # Adjust time step
-    #
+    # 
     def check_time_step(self):
         if self.iter_ >= self.max_iter:
             self.dt = max(0.1, self.dt * 0.7)
         if self.iter_ <= self.min_iter:
             self.dt = min(self.max_delta_t, self.dt * 1.3)
-            
+
+    # Adjust time step
+    # 
     def check_time_step02(self):
         self.dt = max(0.1, self.dt * 0.3)
-        #if self.iter_ >= self.max_iter:
-        #if self.iter_ <= self.min_iter:
-            #self.dt = min(self.max_delta_t, self.dt * 1.3)
-    
+
     # Tranck rill emergency
     # 
     def rill_check(self, rc):  # rc - rill count
