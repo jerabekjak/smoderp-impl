8144527fed4e6a587f75c7f61a022d09367be7b7
jerabekjak <jerabekjak@email.cz>
2018-10-26 14:51:17 +0200

    add exception

--- smoderp2d/exceptions.py
+++ smoderp2d/exceptions.py
@@ -11,3 +11,16 @@ class MaxIterationExceeded(SmoderpError):
 
     def __str__(self):
         return repr(self.msg)
+
+
+
+class NegativeVolumeInRIll(SmoderpError):
+    """Number of iteration exceed max iteration criterion.
+    """
+    def __init__(self, V):
+        self.msg = 'Negative volume goes to rill (V_to_rill = {}).'.format(
+            V
+        )
+
+    def __str__(self):
+        return repr(self.msg)
