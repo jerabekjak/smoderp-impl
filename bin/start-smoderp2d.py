e6a0450ed03d046aa5fbe0dfa8a84cb304791d60
jerabekjak <jerabekjak@email.cz>
2018-11-22 13:04:00 +0100

    fix start-smoderp in bin/

--- bin/start-smoderp2d.py
+++ bin/start-smoderp2d.py
@@ -1,7 +1,12 @@
-﻿#!/usr/bin/python
+#!/usr/bin/env python
 
+import os
+import sys
 
-import smoderp2d.main as sm
+if __name__ == "__main__":
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+    import smoderp2d.main as sm
 
-
-sm.run()
+    sm.run()
+else:
+    sys.exit("Can be run only as standalone program.")
